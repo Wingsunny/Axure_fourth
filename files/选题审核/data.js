b87c261d30691fa3dcad9a9a4c766d5925eb6392
lyy <774250717@qq.com>
2021-11-30 23:23:34 +0800

    axure毕业论文选题系统网页设计

---
+++ files/选题审核/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,_(g,h,i,_(j,k,l,k)),m,[],n,_(h,o),p,[q,r,s,t,u],v,_(w,x,y,z,g,A,B,_(),C,[],D,_(E,F,G,H,I,_(J,K,L,M),N,null,O,H,H,P,Q,R,null,S,T,U,V,W,X,Y,T,Z,ba,_(J,K,L,bb),bc,T,bd,be,_(bf,bg,bh,bi,bj,bi,bk,bi,bl,k,L,_(bm,bn,bo,bn,bp,bn,bq,br)),i,_(j,k,l,k)),bs,_(),bt,_(),bu,_(bv,[])),bw,_(),bx,_());}; 
+var b="url",c="选题审核.html",d="generationDate",e=new Date(1638262776602.69),f="defaultAdaptiveView",g="name",h="",i="size",j="width",k=0,l="height",m="adaptiveViews",n="sketchKeys",o="s0",p="variables",q="OnLoadVariable",r="ok",s="gonggao",t="faburen",u="fabushijian",v="page",w="packageId",x="ae914bba03a64831a4eaf3d584eff96e",y="type",z="Axure:Page",A="选题审核",B="notes",C="annotations",D="style",E="baseStyle",F="627587b6038d43cca051c114ac41ad32",G="pageAlignment",H="near",I="fill",J="fillType",K="solid",L="color",M=0xFFFFFFFF,N="image",O="imageAlignment",P="imageRepeat",Q="auto",R="favicon",S="sketchFactor",T="0",U="colorStyle",V="appliedColor",W="fontName",X="Applied Font",Y="borderWidth",Z="borderVisibility",ba="borderFill",bb=0xFF797979,bc="cornerRadius",bd="cornerVisibility",be="outerShadow",bf="on",bg=false,bh="offsetX",bi=5,bj="offsetY",bk="blurRadius",bl="spread",bm="r",bn=0,bo="g",bp="b",bq="a",br=0.349019607843137,bs="adaptiveStyles",bt="interactionMap",bu="diagram",bv="objects",bw="masters",bx="objectPaths";
+return _creator();
+})());
